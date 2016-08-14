da5b8b608d03a7992f5145d3333822cdad825c3b
Anton Vinogradov <etheralgreen@gmail.com>
2016-08-14 19:13:24 +1100

    Ready homework for 1 2 3 4 5 6 7 8 9 tasks

--- z7.php
+++ z7.php
@@ -9,22 +9,22 @@ mb_internal_encoding('utf-8');
     <title>Домашка 2</title>
 </head>
 <body>
-<?php 
+<?php
 /*
  * Задание #7
  * Дана строка: “Карл у Клары украл Кораллы”. удалить из этой строки все заглавные буквы “К”.
  * Дана строка “Две бутылки лимонада”. Заменить “Две”, на “Три”. По желанию дополнить задание.
  */
-$sting="Карл у Клары украл Кораллы";
-function TestRegularFirst(){
-    $text='Карл у Клары украл кораллы, а Клара у Карла украла кларнет .';
-    echo '<strong>Пример:</strong> '.htmlspecialchars($text);
-    preg_match_all("/(К.*?)\s/",$text,$result);
-    echo '<br/><strong>Результат:</strong> <pre>'.var_export($result[1],true).'</pre>';
-}
-echo TestRegularFirst();
+$text = 'Карл у Клары украл кораллы';
+$regexp= '/К/u';
+$result=preg_replace($regexp, ' ', $text);
+echo $result;
+echo "<br>";
 
-
+$othertext = 'Две бутылки лимонада';
+$regexp= '/Две/u';
+$result=preg_replace($regexp, 'Три', $othertext);
+echo $result;
 ?>
 </body>
 </html>
