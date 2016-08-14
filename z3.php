98eef29182bdabde228926b7db7ae5fded1b08e6
Anton Vinogradov <etheralgreen@gmail.com>
2016-08-14 17:24:40 +1100

    Ready homework for 1 2 3 4 5 6 8 9 tasks

--- z3.php
+++ z3.php
@@ -20,17 +20,84 @@ error_reporting(-1);
 Пример вызова: calcEverything(‘+’, 1, 2, 3, 5.2);
 Результат: 1 + 2 + 3 + 5.2 = 11.2
  */
-    function calcEverything(){
-        print("<ol>\n");
-        for ($i=0;$i<func_num_args(); $i++)
-        {
-            print("<li>" . func_get_arg($i) . "</li>\n");
+    function calcEverything()
+    {
+
+        // Да глупая проверка первый аргумент. Зато работает! И можно в случае чего улучшить
+        for ($i = 0; $i <2; $i++) {
+            if ($i == 0) {
+                $deistv = func_get_arg($i);
+            } else {
+                //echo func_get_arg($i);
+                //echo $deistv;
+                $someAct = func_num_args(1);
+                $primer='';
+                if ($deistv == '+') {
+                    echo "Сложение<br>";
+                    for ($j = 1; $j < func_num_args(); $j++) {
+                        $someAct = $someAct + func_get_arg($j);
+                        if ($j==1)
+                        {
+                            $primer= func_get_arg(1);
+                        }else                        {
+                            $primer=$primer.'+'.func_get_arg($j);
+                        }
+                    }
+                    echo $primer."=".$someAct."<br>";
+                }
+                elseif
+                    ($deistv == '-') {
+                        echo "Вычитание<br>";
+                    for ($j = 1; $j < func_num_args(); $j++) {
+                        $someAct = $someAct - func_get_arg($j);
+                        if ($j==1)
+                        {
+                            $primer= func_get_arg(1);
+                        }else                        {
+                            $primer=$primer.'-'.func_get_arg($j);
+                        }
+                    }
+                    echo $primer."=".$someAct."<br>";
+                    }
+                elseif ($deistv == '*') {
+                        echo "Умножение<br>";
+                    for ($j = 1; $j < func_num_args(); $j++) {
+                        $someAct = $someAct * func_get_arg($j);
+                        if ($j==1)
+                        {
+                            $primer= func_get_arg(1);
+                        }else                        {
+                            $primer=$primer.'*'.func_get_arg($j);
+                        }
+                    }
+                    echo $primer."=".$someAct."<br>";
+                    }
+                elseif ($deistv == '/') {
+                        echo "Деление<br>";
+                    for ($j = 1; $j < func_num_args(); $j++) {
+                        $someAct = $someAct / func_get_arg($j);
+                        if ($j==1)
+                        {
+                            $primer= func_get_arg(1);
+                        }else                        {
+                            $primer=$primer.'/'.func_get_arg($j);
+                        }
+                    }
+                    echo $primer."=".$someAct."<br>";
+                    }
+                else {
+                        echo "Неизвестная операция<br>";
+                    }
+            }
         }
-        print("</ol>\n");
     }
     echo "<br>";
-    echo calcEverything("Linux", "Apache", "Mysql", "PHP");
     echo calcEverything("+", 1, 2, 3, 5.2);
+    echo calcEverything("+", 8, 8, 15, 16, 23, 42, 84,186);
+    echo calcEverything("-", 1, 2, 3, 5.2);
+    echo calcEverything("*", 1, 2, 3, 5.2);
+    echo calcEverything("/", 1, 2, 3, 5.2);
+   // echo calcEverything("+", 1, 2, 3, 5.2);
 ?>
 </body>
 </html>
